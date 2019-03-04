b933a7ad68d97abe03ff8babf83217d33db0a810
yassinekarami <yassine.karami@outlook.com>
2019-03-04 21:46:46 +0000

    commentaire serveur

--- client1.c
+++ client1.c
@@ -18,7 +18,7 @@
 #include <pthread.h>
 #include <arpa/inet.h>
 
-#define PORT "5000" // the port client will be connecting to
+#define PORT "5000" // le port auquel le client sera connecter
 
 #define MAXDATASIZE 250 // max number of bytes we can get at once
 #define MAXNAMESIZE 11
@@ -38,24 +38,29 @@ void *get_in_addr(struct sockaddr *sa)
 
 int main(int argc, char *argv[])
 {
-    char message[MAXDATASIZE];
-    char nickName[MAXNAMESIZE];
+    char message[MAXDATASIZE]; // tableau du message
+    char nickName[MAXNAMESIZE]; // tableau du pseudo
+
     //int *new_sockfd; //added
     int sockfd;//, numbytes;
     //char buf[MAXDATASIZE];
+
     char sBuf[MAXDATASIZE]; //added
+
+    // structure prédéfini dans les biblioteque importé
+    // contient les informations sur un hote internet ou un service
     struct addrinfo hints, *servinfo, *p;
     int rv;
-    char s[INET6_ADDRSTRLEN];
-
+    char s[INET6_ADDRSTRLEN];  // taille de la chaine de caractère pour l'adress  IP
+ 
     if (argc != 2) {
         fprintf(stderr,"usage: client hostname\n");
         exit(1);
     }
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
+    memset(&hints, 0, sizeof hints); // on vide la structure contenant les info sur l'adress
+    hints.ai_family = AF_UNSPEC; // AF_UNSPEC indique que l'adresse sera IPV4 ou IPV6
+    hints.ai_socktype = SOCK_STREAM; // type de socket attendu , Support de dialogue garantissant l'intégrité, fournissant un flux de données binaires
 
     if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
@@ -64,8 +69,7 @@ int main(int argc, char *argv[])
 
     // loop through all the results and connect to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("client: socket");
             continue;
         }
@@ -79,13 +83,13 @@ int main(int argc, char *argv[])
         break;
     }
 
+    // on arrive a pas connecté le client avec la socket
     if (p == NULL) {
         fprintf(stderr, "client: failed to connect\n");
         return 2;
     }
 
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-            s, sizeof s);
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
     printf("client: connecting to %s\n", s);
 
     freeaddrinfo(servinfo); // all done with this structure
--- server1.c
+++ server1.c
@@ -30,48 +30,66 @@ void *get_in_addr(struct sockaddr *sa)
 
 int main(void)
 {
+    // File descriptor indicateur utilisé pour accédé au ressource input/output (socket)
     fd_set master;    //master file descriptor list
     fd_set read_fds;  //temp file descriptor list for select()
     int fdmax;        //max. amount of file descriptors
 
     int listener;     //listening socket descriptor
     int newfd;        //newly accept()ed socket descriptor
-    struct sockaddr_storage remoteaddr; //client address
+
+    struct sockaddr_storage remoteaddr; //client address , defini une socket qu'on pourra manipulé (bind , connect) 
     socklen_t addrlen;
 
     char buf[256];    //buffer for client data
     int nbytes;
 
-    char remoteIP[INET6_ADDRSTRLEN];
+    char remoteIP[INET6_ADDRSTRLEN]; // taille de la chaine de caractère pour l'adress  IP
 
     int yes=1;        //for setsockopt() SO_REUSEADDR, below
     int i, j, rv;
 
-    struct addrinfo hints, *ai, *p;
+    // structure prédéfini dans les biblioteque importé
+    // contient les informations sur un hote internet ou un service
+    struct addrinfo hints, *ai, *p;  
 
-    FD_ZERO(&master);    //FD_ZERO works like memset 0;
-    FD_ZERO(&read_fds);  //clear the master and temp sets
+    FD_ZERO(&master);    //FD_ZERO works like memset 0;  vide la liste master 
+    FD_ZERO(&read_fds);  //clear the master and temp sets   vide la list de lecture
 
     // fetch a socket, bind it
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
+    memset(&hints, 0, sizeof hints); // on vide la structure contenant les info sur l'adress
+    hints.ai_family = AF_UNSPEC;  // AF_UNSPEC indique que l'adresse sera IPV4 ou IPV6
+    hints.ai_socktype = SOCK_STREAM;  // type de socket attendu , Support de dialogue garantissant l'intégrité, fournissant un flux de données binaires
+    hints.ai_flags = AI_PASSIVE; // permet de lié la socket , et la socket va accepté la connection
+
+    // getaddrinfo() renvoie une ou plusieurs structures addrinfo,
+    // chacune d'entre elles contenant une adresse Internet qui puisse être indiquée dans un appel à bind ou connect
+    // hint donne les critères de selection des structures d'adresse de socket
+    // ai pointe sur la liste des critères
+    // on teste si on peut accédé au PORT désiré
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) { 
         fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
         exit(1);
     }
 
+    // on parcourt la liste des structures ( liste chainer , p->ai_next , pointe sur la structure suivante )
+    // on crée une socket pour chaque adress
     for(p = ai; p != NULL; p = p->ai_next)
-    {
-        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    {   
+        // crée un socket , et renvoi un descripteur
+        // le descripteur est une référence a la socket créé
+        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol); 
         if (listener < 0) {
             continue;
         }
 
         // lose the pesky "address already in use" error message
+        // défini les options de la socket
+        // SOL_SOCKET 
+        // SO_REUSEDDR autorise la réutilisation de l'adresse locale
         setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
+        // on lie la socket avec l'adresse
         if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
             close(listener);
             continue;
@@ -80,7 +98,7 @@ int main(void)
         break;
     }
 
-    // if we got here, bondage failed.
+    // si la liaison échoue
     if (p == NULL) {
         fprintf(stderr, "selectserver: failed to bind\n");
         exit(2);
@@ -89,15 +107,18 @@ int main(void)
     freeaddrinfo(ai); //all done; free memory
 
     puts("binding successful!");
-    // listen
+
+    // listen marque la référence de la socket qui est utilisé pour accepter les demandes de connexions entrantes en utilisant accept()
     if (listen(listener, 10) == -1)
     {
         perror("listen");
         exit(3);
     }
+
     puts("hearing aid is on, server listening...");
 
     // add the listener to the master set
+    // on ajoute les descripteurs ( référence de socket ) dans la liste principale
     FD_SET(listener, &master);
 
     // keep track of the biggest file descriptor
@@ -107,6 +128,8 @@ int main(void)
     for(;;)
     {
         read_fds = master; // copy it
+
+        // on verifie que les file descriptor sont prêt a être utilisé ( entré - sortie )
         if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1)
         {
             perror("select");
@@ -114,14 +137,20 @@ int main(void)
         }
 
         //run through the existing connections looking for data to read
+        // parcours les références de socket a la recherche de donné a lire
         for(i = 0; i <= fdmax; i++) //EDIT HERE
         {
+            // on verifie que "i" est bien compris dans la liste des file descriptor
+            // (doc) Returns a non-zero value if the bit for the file descriptor fd is set in the file descriptor set pointed to by fdset, and 0 otherwise.
+
             if (FD_ISSET(i, &read_fds))
             {   // we got one
                 if (i == listener)
                 {
                     // handle new connections
                     addrlen = sizeof remoteaddr;
+
+                    // crée un socket avec les meme caractéristique que sockadrr
                     newfd = accept(listener,
                         (struct sockaddr *)&remoteaddr,
                         &addrlen);
@@ -130,24 +159,34 @@ int main(void)
                     {
                         perror("accept");
                     }
+
                     else
                     {
+                        // on passe le bit de new fd dans la liste des files descriptor
                         FD_SET(newfd, &master); // pass to master set
+
                         if (newfd > fdmax)
                         {   // keep track of the max
                             fdmax = newfd;
                         }
+
                         printf("selectserver: new connection from %s on "
                                "socket %d\n", inet_ntop(remoteaddr.ss_family,
                                get_in_addr((struct sockaddr*)&remoteaddr),
                                remoteIP, INET6_ADDRSTRLEN), newfd);
                     }
                 }
+                
                 else
                 {
                     // gérer les messages envoyés par les clients
+                    // i représente la socket
+                    // buf => tableau de 256 char
+                    // recv utilisé pour recevoir les messages depuis une socket
+                    // renvoie le nombre d'octets reçus
                     if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0)
-                    {   // got error or connection closed by client
+                    {   
+                        // erreur ou connexion fermer par le client
                         if (nbytes == 0)
                         {   // connection closed
                             printf("selectserver: socket %d hung up\n", i);
@@ -161,10 +200,13 @@ int main(void)
                     }
                     else
                     {
-                        // we got some data from a client
+                        // nous avons reçu des données d'un client
+
+                        // fdmax , le nombre de file descriptor ( reférence des sockets )
                         for(j = 0; j <= fdmax; j++)
                         {
                             // broadcast to everyone
+                            // verifie que le bit qui réprente la socket, est present dans la liste des file descriptor (socket)
                             if (FD_ISSET(j, &master))
                             {
                                 // except the listener and ourselves
